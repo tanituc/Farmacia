f66bf81d2e16d2375749bfdedfebf76b2fdea5f3
Fune <rodrigo.funes055@gmail.com>
2017-02-15 01:47:31 -0300

    Rehaciendo BD

---
+++ Farmacia/Vistas/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Farmacia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.medicamentosToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laboratoriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ventasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionMedicamentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionClientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionLaboratoriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevaVentaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ventasToolStripMenuItem,
+            this.medicamentosToolStripMenuItem1,
+            this.clientesToolStripMenuItem,
+            this.laboratoriosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(763, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // medicamentosToolStripMenuItem1
+            // 
+            this.medicamentosToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestionMedicamentosToolStripMenuItem});
+            this.medicamentosToolStripMenuItem1.Name = "medicamentosToolStripMenuItem1";
+            this.medicamentosToolStripMenuItem1.Size = new System.Drawing.Size(98, 20);
+            this.medicamentosToolStripMenuItem1.Text = "Medicamentos";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestionClientesToolStripMenuItem});
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // laboratoriosToolStripMenuItem
+            // 
+            this.laboratoriosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestionLaboratoriosToolStripMenuItem});
+            this.laboratoriosToolStripMenuItem.Name = "laboratoriosToolStripMenuItem";
+            this.laboratoriosToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.laboratoriosToolStripMenuItem.Text = "Laboratorios";
+            // 
+            // ventasToolStripMenuItem
+            // 
+            this.ventasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevaVentaToolStripMenuItem});
+            this.ventasToolStripMenuItem.Name = "ventasToolStripMenuItem";
+            this.ventasToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.ventasToolStripMenuItem.Text = "Ventas";
+            // 
+            // gestionMedicamentosToolStripMenuItem
+            // 
+            this.gestionMedicamentosToolStripMenuItem.Name = "gestionMedicamentosToolStripMenuItem";
+            this.gestionMedicamentosToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.gestionMedicamentosToolStripMenuItem.Text = "Gestion Medicamentos";
+            this.gestionMedicamentosToolStripMenuItem.Click += new System.EventHandler(this.gestionMedicamentosToolStripMenuItem_Click);
+            // 
+            // gestionClientesToolStripMenuItem
+            // 
+            this.gestionClientesToolStripMenuItem.Name = "gestionClientesToolStripMenuItem";
+            this.gestionClientesToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.gestionClientesToolStripMenuItem.Text = "Gestion Clientes";
+            // 
+            // gestionLaboratoriosToolStripMenuItem
+            // 
+            this.gestionLaboratoriosToolStripMenuItem.Name = "gestionLaboratoriosToolStripMenuItem";
+            this.gestionLaboratoriosToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.gestionLaboratoriosToolStripMenuItem.Text = "Gestion Laboratorios";
+            // 
+            // nuevaVentaToolStripMenuItem
+            // 
+            this.nuevaVentaToolStripMenuItem.Name = "nuevaVentaToolStripMenuItem";
+            this.nuevaVentaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nuevaVentaToolStripMenuItem.Text = "Nueva Venta";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 398);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ventasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevaVentaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem medicamentosToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem gestionMedicamentosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionClientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem laboratoriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionLaboratoriosToolStripMenuItem;
+    }
+}
+
